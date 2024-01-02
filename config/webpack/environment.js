e0a834b25b19771788722861b6e25787b5d6bada
EC2 Default User <ec2-user@ip-172-31-21-83.ap-southeast-2.compute.internal>
2024-01-02 08:22:53 +0000

    [Update]6-24

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpark = require('webpark')
+environment.plugins.prepend(
+    'provide',
+    new webpark.providePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Popper: 'popper.js'
+    })
+)
