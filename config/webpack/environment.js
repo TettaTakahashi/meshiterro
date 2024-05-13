9fab194af28e971363eba3f0dba82f56fd1a857d
TettaTakahashi <x4tetta@gmail.com>
2024-05-13 21:40:17 +0000

    git clone

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-const webpark = require('webpark')
-environment.plugins.prepend(
-    'provide',
-    new webpark.providePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-        Popper: 'popper.js'
-    })
-)
